4c6578da5e037028a98911de891e9dcfb35d07b7
deksor <dxceor@gmail.com>
2020-11-01 19:30:36 +0100

    Adding locale detection

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -6,20 +6,38 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+
 class LocaleSubscriber implements EventSubscriberInterface
 {
     // Default locale
     private $defaultLocale;
+    private $params;
 
-    public function __construct($defaultLocale = 'en')
+    public function __construct($defaultLocale = 'en', ContainerBagInterface $params)
     {
         $this->defaultLocale = $defaultLocale;
+        $this->params = $params;
     }
 
     public function onKernelRequest(RequestEvent $event)
     {
         $request = $event->getRequest();
         if (!$request->hasPreviousSession()) {
+            $clientLangs = $request->getLanguages();
+            $serverLangs = $this->params->get('app.locales');
+            
+            if (($max = count($clientLangs)) > 0)
+            {
+                for ($i = 0; $i < $max; $i++)
+                {
+                    if (in_array(substr($clientLangs[$i], 0, 2), $serverLangs))
+                    {
+                        $request->setLocale($clientLangs[$i]);
+                        return;
+                    }
+                }
+            }
             return;
         }
 
