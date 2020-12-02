843261c88d78a69958e7ac762de61ea1494893be
computerguy08 <andyc03789@gmail.com>
2020-12-02 16:26:09 +0000

    new add page

--- assets/add-collection-widget.js
+++ assets/add-collection-widget.js
@@ -4,32 +4,30 @@ for (var n = 0; n < btn.length; n++) {
 	if (btn[n].className == "add-another-collection-widget") {
 		btn[n].onclick = function() {
 			var list = document.getElementById(this.getAttribute("data-list-selector").substr(1));
-
-			// Try to find the counter of the list or use the length of the list
 			var counter = list.getAttribute("data-widget-counter");
-
-			// grab the prototype template
 			var newWidget = list.getAttribute("data-prototype");
 
-			// replace the "__name__" used in the id and name of the prototype
-			// with a unique number
 			newWidget = newWidget.replace(/__name__/g, counter);
-
-			// Increase the counter
 			counter++;
-
-			// And store it, the length cannot be used if deleting widgets is allowed
 			list.setAttribute("data-widget-counter", counter);
 
 			var newDelButton = document.createElement('button');
-			newDelButton.innerText = "Delete";
+			newDelButton.innerText = "Del";
 			newDelButton.onclick = function() {list.removeChild(newElem);return false;};
-
-			// create a new list element and add it to the list
-			var newElem = document.createElement('li');
+			var newElem = document.createElement('tr');
 			newElem.innerHTML = newWidget;
 			newElem.appendChild(newDelButton);
 			list.appendChild(newElem);	
 		};
 	}
 }
+
+var buttons = document.getElementsByTagName("BUTTON");
+    for(i = 0;i < buttons.length-1; i++)
+    {
+        if(buttons[i].className==="delbutton")
+			if(buttons[i].parentElement.className==="bis-image")
+				buttons[i].addEventListener('click', function () { this.parentElement.parentElement.remove(); });
+			else
+				buttons[i].addEventListener('click', function () { this.parentElement.remove(); });
+    }
