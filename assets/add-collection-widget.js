bdfef5344bef782f79e44d64b2447c2d9d99f4c4
computerguy08 <andyc03789@gmail.com>
2021-01-13 22:20:51 +0000

    improved the images section

--- assets/add-collection-widget.js
+++ assets/add-collection-widget.js
@@ -20,6 +20,20 @@ for (var n = 0; n < btn.length; n++) {
 			list.appendChild(newElem);	
 		};
 	}
+	if (btn[n].className == "add-another-image") {
+		btn[n].onclick = function() {
+			var list = document.getElementById(this.getAttribute("data-list-selector").substr(1));
+			var counter = list.getAttribute("data-widget-counter");
+			var newWidget = list.getAttribute("data-prototype");
+			newWidget = newWidget.replace(/__name__/g, counter);
+			counter++;
+			list.setAttribute("data-widget-counter", counter);
+			var newElem = document.createElement('div');
+			newElem.classList.add("addform");
+			newElem.innerHTML = newWidget;
+			list.appendChild(newElem);	
+		};
+	}
 }
 
 var buttons = document.getElementsByTagName("BUTTON");
@@ -30,4 +44,7 @@ var buttons = document.getElementsByTagName("BUTTON");
 				buttons[i].addEventListener('click', function () { this.parentElement.parentElement.remove(); });
 			else
 				buttons[i].addEventListener('click', function () { this.parentElement.remove(); });
+		if(buttons[i].className==="delbutton-img")
+			buttons[i].addEventListener('click', function () { this.parentElement.parentElement.remove(); });
+			
     }
