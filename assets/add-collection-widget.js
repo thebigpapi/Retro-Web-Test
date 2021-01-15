cee3f40b955b8c4081d04ff9e3c98476bf9224e9
computerguy08 <andyc03789@gmail.com>
2021-01-15 16:39:49 +0000

    rewritten JS,mostly works in IE

--- assets/add-collection-widget.js
+++ assets/add-collection-widget.js
@@ -1,50 +1,23 @@
-var btn = document.getElementsByTagName("button");
-
-for (var n = 0; n < btn.length; n++) {
-	if (btn[n].className == "add-another-collection-widget") {
-		btn[n].onclick = function() {
-			var list = document.getElementById(this.getAttribute("data-list-selector").substr(1));
-			var counter = list.getAttribute("data-widget-counter");
-			var newWidget = list.getAttribute("data-prototype");
-
-			newWidget = newWidget.replace(/__name__/g, counter);
-			counter++;
-			list.setAttribute("data-widget-counter", counter);
-
-			var newDelButton = document.createElement('button');
-			newDelButton.innerText = "Del";
-			newDelButton.onclick = function() {list.removeChild(newElem);return false;};
-			var newElem = document.createElement('tr');
-			newElem.innerHTML = newWidget;
-			newElem.appendChild(newDelButton);
-			list.appendChild(newElem);	
-		};
-	}
-	if (btn[n].className == "add-another-image") {
-		btn[n].onclick = function() {
-			var list = document.getElementById(this.getAttribute("data-list-selector").substr(1));
-			var counter = list.getAttribute("data-widget-counter");
-			var newWidget = list.getAttribute("data-prototype");
-			newWidget = newWidget.replace(/__name__/g, counter);
-			counter++;
-			list.setAttribute("data-widget-counter", counter);
-			var newElem = document.createElement('div');
-			newElem.classList.add("addform");
-			newElem.innerHTML = newWidget;
-			list.appendChild(newElem);	
-		};
-	}
+//function to add new widgets; written by computerguy08
+function expand(id) { 
+	//store the table widget DOM in list and newWidget, increment the counter
+	var list = document.getElementById(id);
+	var counter = list.getAttribute("data-widget-counter");
+	var newWidget = list.getAttribute("data-prototype");
+	//alert(newWidget);
+	newWidget = newWidget.replace(/__name__/g, counter);
+	newWidget = newWidget.replace("/>", ">");
+	counter++;
+	//set the new increment, create the new widget and concatenate after list's last child node
+	list.setAttribute("data-widget-counter", counter);
+	var newElem = document.createElement('div');
+	newElem.setAttribute("style", "width:100%"); 
+	//alert(newWidget);
+	newElem.innerHTML = newWidget;
+	//alert(newElem.innerHTML);
+	//alert(list.innerHTML);
+	var c = list.childNodes;
+	//alert(newElem.innerHTML);
+	list.appendChild(newElem);
+	//alert(list.outerHTML);	
 }
-
-var buttons = document.getElementsByTagName("BUTTON");
-    for(i = 0;i < buttons.length-1; i++)
-    {
-        if(buttons[i].className==="delbutton")
-			if(buttons[i].parentElement.className==="bis-image")
-				buttons[i].addEventListener('click', function () { this.parentElement.parentElement.remove(); });
-			else
-				buttons[i].addEventListener('click', function () { this.parentElement.remove(); });
-		if(buttons[i].className==="delbutton-img")
-			buttons[i].addEventListener('click', function () { this.parentElement.parentElement.remove(); });
-			
-    }
