aed366c0061dbab6b6d5ba635440587a421ff324
computerguy08 <andyc03789@gmail.com>
2021-01-16 15:56:51 +0000

    pre-realease of the new add page

--- assets/add-collection-widget.js
+++ assets/add-collection-widget.js
@@ -1,23 +1,18 @@
+/* add-collection-widget.js - most of the JS code runs from here */
+
 //function to add new widgets; written by computerguy08
 function expand(id) { 
 	//store the table widget DOM in list and newWidget, increment the counter
 	var list = document.getElementById(id);
 	var counter = list.getAttribute("data-widget-counter");
 	var newWidget = list.getAttribute("data-prototype");
-	//alert(newWidget);
 	newWidget = newWidget.replace(/__name__/g, counter);
 	newWidget = newWidget.replace("/>", ">");
 	counter++;
-	//set the new increment, create the new widget and concatenate after list's last child node
+	//set the new increment, create the new widget and concatenate after list
 	list.setAttribute("data-widget-counter", counter);
 	var newElem = document.createElement('div');
 	newElem.setAttribute("style", "width:100%"); 
-	//alert(newWidget);
 	newElem.innerHTML = newWidget;
-	//alert(newElem.innerHTML);
-	//alert(list.innerHTML);
-	var c = list.childNodes;
-	//alert(newElem.innerHTML);
-	list.appendChild(newElem);
-	//alert(list.outerHTML);	
+	list.appendChild(newElem);	
 }
