ee4eb5f89e221a5825769edc70def4ac98b23206
computerguy08 <andyc03789@gmail.com>
2021-02-14 21:41:22 +0000

    added null file JS protection (for server error 500), added CPU/FPU/FSB label in show page

--- assets/add-collection-widget.js
+++ assets/add-collection-widget.js
@@ -12,7 +12,34 @@ function expand(id) {
 	//set the new increment, create the new widget and concatenate after list
 	list.setAttribute("data-widget-counter", counter);
 	var newElem = document.createElement('div');
-	newElem.setAttribute("style", "width:100%"); 
+	if(id == 'images-fields-list')
+		newElem.setAttribute("class", "addform");
+	if(id != 'images-fields-list')
+		newElem.setAttribute("style", "width:100%"); 
 	newElem.innerHTML = newWidget;
 	list.appendChild(newElem);	
+}
+// function used to check for null file uploads
+function check_E(){
+	var manuals = document.getElementById('manuals-fields-list');
+	var photos = document.getElementById('images-fields-list');
+	var manual = manuals.children;
+	var photo = photos.children;
+	for (var i = 0; i < manual.length; i++) {
+		if(manual[i].children[2].children[0].files[0] == null){
+			if (manual[i].children[3].children[0].value == ''){
+				alert("One of the file upload fields is empty! (manual entry no." + (i + 1) + ")");
+				return false;
+			}
+		}
+	}
+	for (var i = 0; i < photo.length; i++) {
+		if(photo[i].children[0].children[0].files[0] == null){
+			if (photo[i].children[6].children[0].value == ''){
+				alert("One of the file upload fields is empty! (image entry no." + (i + 1) + ")");
+				return false;
+			}
+		}
+	}
+	return true;
 }
