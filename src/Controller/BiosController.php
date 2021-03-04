055c369af419a1085fbfa947f9d1a5f071563783
deksor <dxceor@gmail.com>
2021-03-04 20:07:16 +0100

    BIOS search working, translations and frontend need some polish

--- src/Controller/BiosController.php
+++ src/Controller/BiosController.php
@@ -17,6 +17,48 @@ use App\Form\Bios\Search;
 
 class BiosController extends AbstractController
 {
+
+    /**
+     * @Route("/bios/result", name="bios_result"), methods={"GET"})
+     * @param Request $request
+     */
+    public function result(Request $request, PaginatorInterface $paginator)
+    {
+        $criterias = array();
+
+        $postString = htmlentities($request->query->get('postString'));
+        if ($postString) $criterias['post_string'] = "$postString";
+
+        $coreVersion = htmlentities($request->query->get('coreVersion'));
+        if ($coreVersion) $criterias['core_version'] = "$coreVersion";
+
+        $biosManufacturerId = htmlentities($request->query->get('biosManufacturerId'));
+        if ($biosManufacturerId && intval($biosManufacturerId)) $criterias['manufacturer_id'] = intval($biosManufacturerId);
+        elseif ($biosManufacturerId == "NULL") $criterias['manufacturer_id'] = NULL;
+
+        $filePresent = htmlentities($request->query->get('filePresent'));
+        if ($filePresent && boolval($filePresent)) $criterias['file_present'] = boolval($filePresent);
+
+        $chipsetId = htmlentities($request->query->get('chipsetId'));
+        if ($chipsetId && intval($chipsetId)) $criterias['chipset_id'] = intval($chipsetId);
+        elseif ($chipsetId == "NULL") $criterias['chipset_id'] = NULL;
+
+        $data = $this->getDoctrine()
+            ->getRepository(MotherboardBios::class)
+            ->findBios($criterias);
+
+        $bios = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            $this->getParameter('app.pagination.max')
+        );
+
+        return $this->render('bios/result.html.twig', [
+            'bios' => $bios,
+            'bios_count' => count($bios),
+        ]);
+    }
+
     /**
      * @Route("/bios/search", name="bios_search"), methods={"GET"})
      * @param Request $request
@@ -49,8 +91,26 @@ class BiosController extends AbstractController
                     'form' => $form->createView(),
                 ]);
             }
+            $parameters = array();
+            //dd($form->getData());
 
+            if($postString = $form['post_string']->getData()) {
+                $parameters['postString'] = $postString;
+            }
+            if($coreVersion = $form['core_version']->getData()) {
+                $parameters['coreVersion'] = $coreVersion;
+            }
+            if($biosManufacturer = $form['manufacturer']->getData()) {
+                $parameters['biosManufacturerId'] = $biosManufacturer->getId();
+            }
+            if($filePresent = $form['file_present']->getData()) {
+                $parameters['filePresent'] = $filePresent;
+            }
+            if($chipset = $form['chipset']->getData()) {
+                $parameters['chipsetId'] = $chipset->getId();
+            }
 
+            return $this->redirect($this->generateUrl('bios_result', $parameters));
         }
         return $this->render('bios/search.html.twig', [
             'form' => $form->createView(),
