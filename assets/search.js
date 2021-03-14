3cbedae2b20bf3cec530162fb7f1cf1a6deb8489
computerguy08 <andyc03789@gmail.com>
2021-03-14 19:57:35 +0000

    search JS fix for bios search page

--- assets/search.js
+++ assets/search.js
@@ -1,6 +1,6 @@
-function setChipset(ok) {
-    var chipManuf = document.getElementById("search_motherboard_chipsetManufacturer");
-    var form = document.getElementsByName("search_motherboard")[0];
+function setChipset(ok, formtype) {
+    var chipManuf = document.getElementById(formtype + "_chipsetManufacturer");
+    var form = document.getElementsByName(formtype)[0];
     if (window.XMLHttpRequest)
         var xhttp = new XMLHttpRequest();
     else if (window.ActiveXObject)
@@ -22,23 +22,33 @@ function setChipset(ok) {
     };
 	var chipsetManufacturer = chipManuf.value;
 	if(ok)var chipsetManufacturer = "";
-    var params = "search_motherboard[chipsetManufacturer]="+chipsetManufacturer;
+    var params = formtype + "[chipsetManufacturer]="+chipsetManufacturer;
     xhttp.open('POST', form.action, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(params);
 }
+
+/* execution starts HERE; detect the selects and determine the type of search page*/
 var lb1 = document.getElementById('setchip1');
 var lb2 = document.getElementById('setchip2');
+if (document.getElementsByName("search_motherboard")[0])
+	var formtype = "search_motherboard";
+else
+	var formtype = "search";
+var chip = document.getElementById(formtype + '_chipsetManufacturer');
+var rst = document.getElementById('rst-btn');
+
+/* compatibility for IE JS*/
 if (window.XMLHttpRequest)
     var xhttp = new XMLHttpRequest();
 else if (window.ActiveXObject)
     var xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 if (xhttp) { 
-    document.getElementById('search_motherboard_searchChipsetManufacturer').outerHTML="";
+    document.getElementById(formtype + '_searchChipsetManufacturer').outerHTML="";
 	lb2.style.width="70%";
 }
-var chip = document.getElementById('search_motherboard_chipsetManufacturer');
-var rst = document.getElementById('rst-btn');
+
+/* event listeners*/
 function addEvent(evnt, elem, func){
     if(elem.addEventListener)
         elem.addEventListener(evnt,func,false);
@@ -48,12 +58,12 @@ function addEvent(evnt, elem, func){
     else elem["on"+evnt] = func;
 }
 rst.onclick= function() {
-	setChipset(1);
+	setChipset(1,formtype);
 	lb1.style.display="";
 	lb2.style.display="none";
 };
 chip.onchange= function() {
-	setChipset(0);
+	setChipset(0,formtype);
 	lb1.style.display="";
 	lb2.style.display="none";
 };
