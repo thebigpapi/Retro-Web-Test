d8ded9ed32a9b7881654dcac05a2b8287503abc3
computerguy08 <andyc03789@gmail.com>
2021-03-15 16:31:12 +0000

    newer db migration version

---
+++ src/Migrations/Version20210315155456.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210315155456 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE license_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE creditor_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE known_issue_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE motherboard_image_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE manufacturer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE processor_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE manual_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE processor_platform_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE language_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE chipset_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE motherboard_image_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE motherboard_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE max_ram_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE cache_size_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE cpu_speed_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE motherboard_alias_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE video_chipset_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE motherboard_bios_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE chipset_part_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE form_factor_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE coprocessor_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE io_port_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE audio_chipset_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE expansion_slot_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE dram_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE license_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE creditor_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE known_issue_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE motherboard_image_type_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE manufacturer_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE processor_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE manual_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE processor_platform_type_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE language_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE chipset_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE motherboard_image_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE motherboard_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE max_ram_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE cache_size_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
+        $this->addSql('DROP SEQUENCE cpu_speed_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE motherboard_alias_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE video_chipset_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE motherboard_bios_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE chipset_part_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE form_factor_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE coprocessor_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE io_port_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE audio_chipset_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE expansion_slot_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE dram_type_id_seq CASCADE');
+    }
+}
