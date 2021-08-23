5fcde092937064c34de6814104f20698b76ccc7e
BigMonstro <bigmonstro@email.com>
2021-08-23 02:23:21 +0200

    Large dropdown lists readable on low resolutions

--- assets/search.js
+++ assets/search.js
@@ -2,7 +2,7 @@ function setChipset(ok, formtype, sel1, sel2, sel_lb, typ, chk) {
     var chipManuf = document.getElementById(sel1).childNodes;
 	var lb1 = document.getElementById(sel_lb);
 	var lb2 = document.getElementById(sel2);
-	lb1.style.display="";
+	lb1.innerHTML = ls;
 	lb2.style.display="none";
 	if(chk)
     	var form = document.getElementsByName(formtype + "_motherboard")[0];
@@ -17,13 +17,13 @@ function setChipset(ok, formtype, sel1, sel2, sel_lb, typ, chk) {
             var currentForm = document.getElementById(sel2);
             var parser = document.getElementById('hiddenDiv');
 			var lb1 = document.getElementById(sel_lb);
-			lb1.style.display="none";
+			lb1.innerHTML="";
 			currentForm.style.display="";
-			currentForm.style.width="100%";
             parser.innerHTML = xhttp.responseText;
             var doc = document.getElementById(sel2);
             currentForm.innerHTML =  doc.innerHTML;
             parser.innerHTML="";
+			setMinWidth(sel1, sel2);
         }
     };
 	
@@ -33,6 +33,23 @@ function setChipset(ok, formtype, sel1, sel2, sel_lb, typ, chk) {
     xhttp.open('POST', form.action, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(params);
+}
+function setMinWidth(sel1, sel2, chk) {
+	if (!document.getElementsByClassName || (document.all && !window.atob)) {return;}
+	if(chk){
+		document.getElementById(sel1).getElementsByTagName('select')[0].style.minWidth =
+		document.getElementById("setchip1").getElementsByTagName('select')[0].offsetWidth + 'px';
+	}
+	if (document.getElementById(sel1).offsetTop == document.getElementById(sel2).parentNode.offsetTop) {
+		document.getElementById(sel2).getElementsByTagName('select')[0].style.minWidth =
+		document.getElementById(sel1).parentNode.parentNode.offsetWidth * 0.9 - 7
+		- document.getElementById(sel1).offsetWidth + 'px';
+		document.getElementById(sel2).parentNode.style.padding='';
+	} else {
+		document.getElementById(sel2).getElementsByTagName('select')[0].style.minWidth =
+		document.getElementById(sel1).getElementsByTagName('select')[0].offsetWidth + 'px';
+		document.getElementById(sel2).parentNode.style.padding='8px 0';
+	}
 }
 
 /* execution starts HERE; detect the selects and determine the type of search page*/
@@ -56,13 +73,13 @@ if (window.XMLHttpRequest)
 else if (window.ActiveXObject)
     var xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 if (xhttp) { 
-    document.getElementById(formtype + '_searchChipsetManufacturer').outerHTML="";
-	lb2.style.width="100%";
+	document.getElementById(formtype + '_searchChipsetManufacturer').parentNode.innerHTML="";
+	setMinWidth("setchip1", "setchip2");
 	if(chk){
-		document.getElementById(formtype + '_searchSocket1').outerHTML="";
-		document.getElementById(formtype + '_searchSocket2').outerHTML="";
-		lb3.style.width="100%";
-		lb4.style.width="100%";
+		document.getElementById(formtype + '_searchSocket1').parentNode.innerHTML="";
+		document.getElementById(formtype + '_searchSocket2').parentNode.innerHTML="";
+		setMinWidth("setcpu1", "setcpu2", chk);
+		setMinWidth("setcpu3", "setcpu4", chk);
 	}
 }
 rst.onclick= function() {
@@ -82,4 +99,11 @@ if(chk){
 	cpu2.onchange= function() {
 		setChipset(0,formtype, "setcpu3", "setcpu4", "setcpu2-lb", "[cpuSocket2]=", chk);
 	};
+}
+window.onresize = function() {
+	setMinWidth("setchip1", "setchip2");
+	if(chk){
+		setMinWidth("setcpu1", "setcpu2", chk);
+		setMinWidth("setcpu3", "setcpu4", chk);
+	}
 }
