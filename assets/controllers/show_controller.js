d36542e38b470222954841390528da6a795ef9e6
computerguy08 <andyc03789@gmail.com>
2021-12-30 23:55:49 +0200

    moved some CSS styling in webpack JS (for no-JS fallback)

--- assets/controllers/show_controller.js
+++ assets/controllers/show_controller.js
@@ -2,6 +2,8 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     connect() {
+        document.getElementById('sh-downloads').style.display = 'none';
+	    document.getElementById('sh-cpus').style.display = 'none';
         let URL = window.location.href;
         if (URL.indexOf("#downloads") != -1)
             this.show_downloads();
