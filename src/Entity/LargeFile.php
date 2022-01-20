88b9842de2cec547e1e97a3d4b11b83ce663afeb
deksor <deksor@gmail.com>
2022-01-20 23:26:12 +0100

    Moved LargeFile's imprecise date handling to a trait so it can be reused for other entities

--- src/Entity/LargeFile.php
+++ src/Entity/LargeFile.php
@@ -2,8 +2,8 @@
 
 namespace App\Entity;
 
+use App\Entity\Traits\ImpreciseDateTrait;
 use App\Repository\LargeFileRepository;
-use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -18,6 +18,7 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
  */
 class LargeFile
 {
+    use ImpreciseDateTrait;
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -105,16 +106,6 @@ class LargeFile
      * @ORM\Column(type="string", length=2048, nullable=true)
      */
     private $note;
-
-    /**
-     * @ORM\Column(type="date", nullable=true)
-     */
-    private $releaseDate;
-
-    /**
-     * @ORM\Column(type="string", length=1, nullable=true)
-     */
-    private $datePrecision;
 
     public function __construct()
     {
@@ -424,72 +415,6 @@ class LargeFile
     public function setNote(?string $note): self
     {
         $this->note = $note;
-
-        return $this;
-    }
-
-    public function getReleaseDate(): ?\DateTimeInterface
-    {
-        return $this->releaseDate;
-    }
-
-    public function setReleaseDate(?\DateTimeInterface $releaseDate): self
-    {
-        //dd($releaseDate);
-        $date = new DateTime();
-
-        switch ($this->getDatePrecision()) {
-            case "m":
-                $date->setDate($releaseDate->format("Y"), $releaseDate->format("m"), "1");
-                break;
-            case "y":
-                $date->setDate($releaseDate->format("Y"), "1", "1");
-                break;
-            default:
-                $date->setDate($releaseDate->format("Y"), $releaseDate->format("m"), $releaseDate->format("d"));
-        }
-        $this->releaseDate = $date;
-
-        return $this;
-    }
-
-    public function getReleaseDateString(): ?string
-    {
-        if ($this->releaseDate) {
-            switch ($this->getDatePrecision()) {
-                case "m":
-                    return $this->releaseDate->format("Y-m");
-                    break;
-                case "y":
-                    return $this->releaseDate->format("Y");
-                    break;
-                default:
-                    return $this->releaseDate->format("Y-m-d");
-            }
-        } else return null;
-    }
-
-    public function getDatePrecision(): ?string
-    {
-        return $this->datePrecision;
-    }
-
-    public function setDatePrecision(?string $datePrecision): self
-    {
-        switch ($datePrecision) {
-            case "m":
-                $char = "m";
-                break;
-            case "y":
-                $char = "y";
-                break;
-            default:
-                $char = "d";
-        }
-
-        $this->datePrecision = $char;
-
-        $this->setReleaseDate($this->getReleaseDate());
 
         return $this;
     }
