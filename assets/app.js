ace97d1455ab759356668e4f7b2f1bae32233b54
computerguy08 <andyc03789@gmail.com>
2022-02-04 19:37:17 +0200

    fixed SEO issues

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import GLightbox from 'glightbox';
+import GLightbox from './controllers/glightbox.min.js';
 
 const lightbox = GLightbox({
     touchNavigation: true,
---
+++ assets/glightbox.js
@@ -0,0 +1,2 @@
+import './styles/glightbox.min.css';
+
