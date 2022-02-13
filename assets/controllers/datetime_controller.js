21dd1c24732766f0e69b75c8b216416242dd3933
deksor <deksor@gmail.com>
2022-02-13 23:22:42 +0100

    JS improvements

--- assets/controllers/datetime_controller.js
+++ assets/controllers/datetime_controller.js
@@ -4,20 +4,16 @@ export default class extends Controller {
     connect() {
         this.changeTime("index-datetime")
     }
-    changeTime(param){
-        let list = document.getElementById(param).children
-        for (let i = 0; i < list.length; i++) {
-            let board = list[i].children[0].innerHTML;
-            let date = new Date(board.substring(0,board.indexOf("|")));
-            let month = ((date.getMonth())<9? '0' : '') + (date.getMonth() + 1);
-            let day = ((date.getDate())<10? '0' : '') + (date.getDate());
-            let hours = ((date.getHours())<10? '0' : '') + (date.getHours());
-            let minutes = ((date.getMinutes())<10? '0' : '') + (date.getMinutes());
-            let timezone = date.toString().substring(date.toString().indexOf("G"),date.toString().indexOf("G")+8);
-            let new_date = date.getFullYear() + "-" + month + "-" + day + ", " + hours + ":" + minutes + " " + timezone;
-            list[i].children[0].innerHTML = new_date;
-            if(document.getElementById(param).nodeName != "DIV")
-                list[i].children[0].innerHTML +=  " " + board.substring(board.indexOf("|"));
+
+    changeTime(id) {
+        let list = document.getElementById(id).children
+        for (let element of list) {
+            let board = element.children[0].innerHTML;
+            let date = new Date(board.substring(0, board.indexOf("|")));
+
+            element.children[0].innerHTML = date.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName:'short'});
+            if (document.getElementById(id).nodeName != "DIV")
+                element.children[0].innerHTML += " " + board.substring(board.indexOf("|"));
         }
     }
 }
