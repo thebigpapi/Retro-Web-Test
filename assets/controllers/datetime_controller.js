e6f2bbeb44323cb4b6b02e7ecd01c9bbbe1866f1
deksor <deksor@gmail.com>
2022-02-14 23:31:13 +0100

    Tweaked the motherboard stats in order to run quicker and fix two small bugs

--- assets/controllers/datetime_controller.js
+++ assets/controllers/datetime_controller.js
@@ -10,8 +10,8 @@ export default class extends Controller {
         for (let element of list) {
             let board = element.children[0].innerHTML;
             let date = new Date(board.substring(0, board.indexOf("|")));
-
-            element.children[0].innerHTML = date.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName:'short'});
+            console.log(navigator.language);
+            element.children[0].innerHTML = date.toLocaleString(navigator.language, {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName:'short'});
             if (document.getElementById(id).nodeName != "DIV")
                 element.children[0].innerHTML += " " + board.substring(board.indexOf("|"));
         }
