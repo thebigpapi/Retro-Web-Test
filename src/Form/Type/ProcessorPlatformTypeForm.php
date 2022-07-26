72b97a5dc77dbbf287a3a630c84287b0f2a48605
computerguy096 <andyc03789@gmail.com>
2022-07-26 19:09:37 +0300

    fixed more sorting

--- src/Form/Type/ProcessorPlatformTypeForm.php
+++ src/Form/Type/ProcessorPlatformTypeForm.php
@@ -6,6 +6,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\ProcessorPlatformType;
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 
 class ProcessorPlatformTypeForm extends AbstractType
 {
@@ -37,5 +40,11 @@ class ProcessorPlatformTypeForm extends AbstractType
     public function getParent(): ?string
     {
         return EntityType::class;
+    }
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        usort($view->vars['choices'], function (ChoiceView $a, ChoiceView $b) {
+            return strnatcasecmp($a->data->getName(), $b->data->getName());
+        });
     }
 }
