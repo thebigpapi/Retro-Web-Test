dc76f7b6d20fe2f1b1758055982d542e6e8989d2
computerguy096 <andyc03789@gmail.com>
2022-07-26 15:48:08 +0300

    fixed known issues markup

--- migrations/Version20220508171831.php
+++ migrations/Version20220508171831.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220606172723 extends AbstractMigration
+final class Version20220508171831 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,15 @@ final class Version20220606172723 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        //$this->addSql('DROP TABLE import');
-        $this->addSql('ALTER TABLE chipset ALTER description TYPE VARCHAR(8192)');
-        $this->addSql('ALTER TABLE chipset_part ALTER description TYPE VARCHAR(8192)');
-        //$this->addSql('ALTER TABLE known_issue ADD description VARCHAR(512) DEFAULT NULL');
+        $this->addSql('DROP TABLE import');
+        $this->addSql('ALTER TABLE known_issue ADD description VARCHAR(512) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        //$this->addSql('CREATE TABLE import (id INT DEFAULT NULL, file_name VARCHAR(255) DEFAULT NULL)');
-        $this->addSql('ALTER TABLE chipset_part ALTER description TYPE VARCHAR(4096)');
-        $this->addSql('ALTER TABLE chipset ALTER description TYPE VARCHAR(4096)');
+        $this->addSql('CREATE TABLE import (id INT DEFAULT NULL, file_name VARCHAR(255) DEFAULT NULL)');
         $this->addSql('ALTER TABLE known_issue DROP description');
     }
 }
