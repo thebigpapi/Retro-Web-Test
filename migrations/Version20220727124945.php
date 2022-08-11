c8c2bd70a75f9cc9d9253bdcb69f55443c8c82fd
deksor <dxceor@gmail.com>
2022-08-11 16:34:42 +0000

    Migration that created template isn't there anymore.

--- migrations/Version20220727124945.php
+++ migrations/Version20220727124945.php
@@ -21,7 +21,6 @@ final class Version20220727124945 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE large_file ADD size INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE motherboard DROP template');
     }
 
     public function down(Schema $schema): void
@@ -29,6 +28,5 @@ final class Version20220727124945 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE large_file DROP size');
-        $this->addSql('ALTER TABLE motherboard ADD template BOOLEAN DEFAULT NULL');
     }
 }
