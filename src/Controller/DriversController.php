02015cb94d5272e362ff73070819156dcf1f6562
deksor <deksor@gmail.com>
2022-08-20 01:49:35 +0200

    Replaced anotations for attributes for DriversController.php

--- src/Controller/DriversController.php
+++ src/Controller/DriversController.php
@@ -11,13 +11,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DriversController extends AbstractController
 {
-    /**
-     * @Route("/drivers/{id}", name="driver_show", requirements={"id"="\d+"})
-     */
+    #[Route('/drivers/{id}', name:'driver_show', requirements:['id'=>'\d+'])]
     public function show(int $id, LargeFileRepository $driverRepository): Response
     {
         $driver = $driverRepository->find($id);
@@ -34,10 +31,7 @@ class DriversController extends AbstractController
         }
     }
 
-    /**
-     * @Route("/drivers/", name="driversearch", methods={"GET"})
-     * @param Request $request
-     */
+    #[Route('/drivers/', name:'driversearch', methods:['GET'])]
     public function searchResult(Request $request, PaginatorInterface $paginator, LargeFileRepository $driverRepository): Response
     {
         $criterias = array();
@@ -66,37 +60,24 @@ class DriversController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/drivers/search/", name="driver_search")
-     * @param Request $request
-     */
-    public function search(Request $request, TranslatorInterface $translator):Response
+    #[Route('/drivers/search/', name:'driver_search')]
+    public function search(Request $request):Response
     {
         $form = $this->createForm(Search::class, array());
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            return $this->redirect($this->generateUrl('driversearch', $this->searchFormToParam($request, $form)));
+            return $this->redirect($this->generateUrl('driversearch', ['name' => $form['name']->getData()]));
         }
         return $this->render('drivers/search.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
-    private function searchFormToParam(Request $request, $form): array
-    {
-        $parameters = array();
-        $parameters['name'] = $form['name']->getData();
-        return $parameters;
-    }
-
-    /**
-     * @Route("/drivers/index/{letter}", name="driverindex", requirements={"letter"="\w|[?]"}), methods={"GET"})
-     * @param Request $request
-     */
+    #[Route('/drivers/index/{letter}', name:'driverindex', requirements:['letter'=>'\w|[?]'], methods:["GET"])]
     public function index(Request $request, PaginatorInterface $paginator, string $letter, LargeFileRepository $driverRepository): Response
     {
-        if ($letter == "?") $letter = "";
+        if ($letter === "?") $letter = "";
         $data = $driverRepository->findAllAlphabetic($letter);
 
         usort(
