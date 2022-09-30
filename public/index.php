5b409d210a71a7b99c657ea177ef33567dac8798
computerguy096 <computerguy096@gmail.com>
2022-09-30 23:11:56 +0000

    first commit on legacy branch

--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
+// yo
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+
