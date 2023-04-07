93b437532c74e13919412ce55ed6633296be8c5f
computerguy096 <computerguy096@gmail.com>
2023-04-07 08:40:49 -0400

    deleted ghost file

--- fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for tbl in `psql -qAt -c "select tablename from pg_tables where schemaname = 'public';" uh19db2` ; do  psql -c "select setval('${tbl}_id_seq', (select max(id) from $tbl))
- " uh19db2 ; done
