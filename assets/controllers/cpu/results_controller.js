cca00c166ab36f7ab95390538972726df75bcee5
computerguy096 <computerguy096@gmail.com>
2023-04-09 13:38:03 -0400

    fixed view aliases button bug, changed socket label

--- assets/controllers/cpu/results_controller.js
+++ assets/controllers/cpu/results_controller.js
@@ -4,10 +4,11 @@ export default class extends Controller {
     connect() {
         let list = document.getElementById('cpu-results-table');
         for (let i=1; i< list.children.length;i++) {
-            let button = list.children[i].children[1];
+            let button = list.children[i].children[1].children[1];
             if(button){
                 button.addEventListener("click", function() {
-                    let content = this.children[2];
+                    this.classList.toggle("active");
+                    let content = button.nextElementSibling;
                     if (content.getAttribute("class") == "cpu-aliases-box"){
                         content.setAttribute("class", "cpu-aliases-box visible");
                     } else {
