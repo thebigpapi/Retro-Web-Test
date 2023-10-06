410cf615ee4d072b6265eda2c79bd72c4b7f60c4
computerguy096 <computerguy096@gmail.com>
2023-10-06 10:11:49 -0400

    added a bios CSV parser

--- assets/webpack/updatecpus.js
+++ assets/webpack/updatecpus.js
@@ -1,9 +1,9 @@
-let updatecpubtn = document.getElementById('update-cpu-btn');
-if(updatecpubtn)
+if(updatecpubtn = document.getElementById('update-cpu-btn'))
     updatecpubtn.addEventListener("click", updateFamilies);
-let slugbtn = document.getElementById('get-slug-btn');
-if(slugbtn)
+if(slugbtn = document.getElementById('get-slug-btn'))
     slugbtn.addEventListener("click", getslug);
+if(mobobios = document.getElementsByClassName('mobo-bios')[0])
+    addFileInput(mobobios);
 function getslug() {
     let manuf = document.getElementById('Motherboard_manufacturer');
     let name = document.getElementById('Motherboard_name');
@@ -22,10 +22,18 @@ function updateFamilies() {
     let cnt = 0;
     // read CPU sockets
     let sockets = document.getElementById('Motherboard_cpuSockets');
-    if(!sockets)
+    if(!sockets){
+        sockets = document.getElementById('Motherboard_cpuSockets_collection').children[0].children[0];
+        if(sockets.innerHTML == "Empty"){
+            alert("Error: no sockets added!");
+            return;
+        }
         sockets = document.getElementById('Motherboard_cpuSockets_collection').children[0].children[0].children[0].children[0];
-    if(sockets.childElementCount == 0)
+    }
+    if(sockets.childElementCount == 0){
+        alert("Error: no sockets added!");
         return;
+    }
     let socket_cnt = sockets.childElementCount;
     while(socket_cnt > 0){
         if(document.getElementById("Motherboard_cpuSockets_" + cnt)){
@@ -36,8 +44,10 @@ function updateFamilies() {
         }
         cnt++;
     }
-    if(params.length == 0)
+    if(params.length == 0){
+        alert("Error: no sockets selected!");
         return;
+    }
     // fetch new platforms based on sockets
     let post = JSON.stringify(params)
     let platformArray = {};
@@ -59,10 +69,18 @@ function updateFamilies() {
 function updateWidget(platformArray){
     let cnt = 0;
     let platforms = document.getElementById('Motherboard_processorPlatformTypes');
-    if(!platforms)
+    if(!platforms){
+        platforms = document.getElementById('Motherboard_processorPlatformTypes_collection').children[0].children[0];
+        if(platforms.innerHTML == "Empty"){
+            alert("Warning: no families added!");
+            return;
+        }
         platforms = document.getElementById('Motherboard_processorPlatformTypes_collection').children[0].children[0].children[0].children[0];
-    if(platforms.childElementCount == 0)
+    }
+    if(platforms.childElementCount == 0){
+        alert("Warning: no families added!");
         return;
+    }
     let platform_cnt = platforms.childElementCount;
     while(platform_cnt > 0){
         if(document.getElementById("Motherboard_processorPlatformTypes_" + cnt)){
@@ -100,4 +118,112 @@ function updatePrototype(platformArray){
     const serializer = new XMLSerializer();
     const document_fragment_string = serializer.serializeToString(doc);
     widget.setAttribute('data-prototype', document_fragment_string)
+}
+function addFileInput(element){
+    let div = document.createElement('div');
+    let input = document.createElement('input');
+    let button = document.createElement('button');
+    input.setAttribute('type', 'file');
+    input.setAttribute('id', 'bios-bot-input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'btn btn-secondary');
+    button.innerHTML = "Get strings";
+    button.addEventListener("click", readFile)
+    div.innerHTML = "CSV ";
+    div.setAttribute('class', 'bios-bot-csv-box');
+    div.appendChild(input);
+    div.appendChild(button);
+    element.firstChild.appendChild(div);
+}
+function readFile(){
+    if(confirm("BIOS strings are about to be changed, proceed?")){
+        //do things
+        let fr=new FileReader();
+        let input = document.getElementById('bios-bot-input');
+        fr.onload=function(){
+            csv=csvToArray(fr.result);
+            if(csv.length < 2){
+                alert("Error: CSV is empty!");
+                return;
+            }
+            processFile(csv);
+        }
+        if(!input.files[0]){
+            alert("Error: no file selected!");
+            return;
+        }
+        let extension = input.files[0].name.split('.').pop().toLowerCase();
+        if(extension != "csv"){
+            alert("Error: not a CSV file!");
+            return;
+        }
+        fr.readAsText(input.files[0]);
+    }
+}
+function processFile(csv){
+    let bioses = document.getElementById('Motherboard_motherboardBios');
+    if(!bioses){
+        bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0];
+        if(bioses.innerHTML == "Empty"){
+            alert("Error: no BIOSes added!");
+            return;
+        }
+        bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0].children[0].children[0];
+    }
+    if(bioses.childElementCount == 0){
+        alert("Error: no BIOSes added!")
+        return;
+    }
+    if(bioses.childElementCount < csv.length){
+        alert("Error: there are more BIOSes in the CSV than in the editor!\nCSV: " + csv.length + "\neditor: " + bioses.childElementCount)
+        return;
+    }
+    let bios_cnt = bioses.childElementCount;
+    let cnt = 0;
+    while(bios_cnt > 0){
+        //read file name
+        if(file = document.getElementById("Motherboard_motherboardBios_" + cnt + "_romFile_file_new_file_name")){
+            let name = file.innerHTML.split(" (")[0];
+            if(name == ""){
+                if(link = document.getElementById("Motherboard_motherboardBios_" + cnt + "_romFile_file_link"))
+                    name = link.getAttribute('title');
+            }
+            if(name != ""){
+                let found = csv.find(item => item[0].includes(name));
+                    /*function(str) {
+                        return str.indexOf(name);
+                    }
+                );*/
+                console.log(name, found);
+                if(found){
+                    document.getElementById("Motherboard_motherboardBios_" + cnt + "_postString").value = found[3].slice(1,-1);
+                    document.getElementById("Motherboard_motherboardBios_" + cnt + "_boardVersion").value = found[4].slice(1,-1);
+                    document.getElementById("Motherboard_motherboardBios_" + cnt + "_coreVersion").value = found[2].slice(1,-1);
+                    let select = document.getElementById("Motherboard_motherboardBios_" + cnt + "_manufacturer");
+                    const options = Array.from(select.options);
+                    const optionToSelect = options.find(item => found[1].slice(1,-1).includes(item.text));
+                    select.value = optionToSelect.value;
+                    select.tomselect.sync();
+                }
+            }
+            bios_cnt--;
+        }
+        cnt++;
+    }
+}
+function csvToArray(csv) {
+    let rows = csv.split('\"\n');
+    // remove the header, not needed
+    rows = rows.slice(1);
+    let result = [];
+    for (const row of rows) {
+        let values = row.split(',');
+        // remove metadata and ROMs, not needed
+        values.pop();
+        values.pop();
+        result.push(values);
+    }
+    // remove last element (empty array)
+    result.pop();
+    return result;
 }
