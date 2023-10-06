9ed24a1943c9b1288557d9b897d66bc213a32123
computerguy096 <computerguy096@gmail.com>
2023-10-06 12:28:48 -0400

    added bulk BIOS upload

--- assets/webpack/updatecpus.js
+++ assets/webpack/updatecpus.js
@@ -121,23 +121,79 @@ function updatePrototype(platformArray){
 }
 function addFileInput(element){
     let div = document.createElement('div');
+    let stringdiv = document.createElement('div');
+    let multifilediv = document.createElement('div');
     let input = document.createElement('input');
+    let multiinput = document.createElement('input');
     let button = document.createElement('button');
+    let multibutton = document.createElement('button');
+    // CSV input
     input.setAttribute('type', 'file');
     input.setAttribute('id', 'bios-bot-input');
     button.setAttribute('type', 'button');
     button.setAttribute('class', 'btn btn-secondary');
     button.innerHTML = "Get strings";
-    button.addEventListener("click", readFile)
-    div.innerHTML = "CSV ";
+    button.addEventListener("click", readFileAsk);
+    stringdiv.innerHTML = "<span>CSV input</span>";
+    stringdiv.appendChild(input);
+    stringdiv.appendChild(button);
+    div.appendChild(stringdiv);
+    // bulk upload
+    multiinput.setAttribute('type', 'file');
+    multiinput.setAttribute('id', 'bios-bulk-upload');
+    multiinput.setAttribute('multiple', '');
+    multibutton.setAttribute('type', 'button');
+    multibutton.setAttribute('class', 'btn btn-secondary');
+    multibutton.innerHTML = "Set files";
+    multibutton.addEventListener("click", bulkUpload);
+    multifilediv.innerHTML = "<span>Bulk upload</span>";
+    multifilediv.appendChild(multiinput);
+    multifilediv.appendChild(multibutton);
     div.setAttribute('class', 'bios-bot-csv-box');
-    div.appendChild(input);
-    div.appendChild(button);
+    div.appendChild(multifilediv);
     element.firstChild.appendChild(div);
 }
+function bulkUpload(){
+    let add = document.getElementById('Motherboard_motherboardBios_collection').previousElementSibling;
+    let input = document.getElementById('bios-bulk-upload');
+    let bioses = document.getElementById('Motherboard_motherboardBios');
+    if(!bioses){
+        bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0];
+        if(bioses.innerHTML != "Empty"){
+            bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0].children[0].children[0];
+        }
+    }
+    if(bioses.childElementCount != 0){
+        alert("Error: BIOS section must be empty!")
+        return;
+    }
+    console.log(input.files);
+    let cnt = 1;
+    for (let i = 0; i < input.files.length; i++) {
+        add.click();
+        const file = input.files[i]
+        const dt = new DataTransfer();
+        bios = document.getElementById('Motherboard_motherboardBios_' + cnt + '_romFile_file');
+        while(!bios){
+            cnt++;
+            bios = document.getElementById('Motherboard_motherboardBios_' + cnt + '_romFile_file');
+        }
+        label = document.getElementById('Motherboard_motherboardBios_' + cnt + '_romFile_file_new_file_name');
+        label.innerHTML = input.files[i].name;
+        dt.items.add(file);
+        bios.files = dt.files;
+        cnt++;
+    }
+    let csvinput = document.getElementById('bios-bot-input');
+    if(input.files[0]){
+        readFile()
+    }
+}
+function readFileAsk(){
+    if(confirm("BIOS strings are about to be changed, proceed?"))
+        readFile();
+}
 function readFile(){
-    if(confirm("BIOS strings are about to be changed, proceed?")){
-        //do things
         let fr=new FileReader();
         let input = document.getElementById('bios-bot-input');
         fr.onload=function(){
@@ -158,7 +214,6 @@ function readFile(){
             return;
         }
         fr.readAsText(input.files[0]);
-    }
 }
 function processFile(csv){
     let bioses = document.getElementById('Motherboard_motherboardBios');
