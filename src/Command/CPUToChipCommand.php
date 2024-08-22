2283946eee0b5665db78b44a2059d249a9267ac9
computerguy096 <computerguy096@gmail.com>
2024-08-22 09:06:46 -0400

    more command improvements

--- src/Command/CPUToChipCommand.php
+++ src/Command/CPUToChipCommand.php
@@ -3,8 +3,9 @@
 namespace App\Command;
 
 use App\Entity\Processor;
-//use App\Repository\ProcessorRepository;
+use App\Repository\ProcessorRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -18,7 +19,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class CPUToChipCommand extends Command
 {
     public function __construct(
-        //private ProcessorRepository $processorRepository,
+        private ProcessorRepository $processorRepository,
         private EntityManagerInterface $entityManagerInterface
     )
     {
@@ -33,7 +34,7 @@ class CPUToChipCommand extends Command
         $processors = $this->processorRepository->findAll();
 
         foreach($processors as $processor) {
-            $this->toJson($processor);
+            $this->toJson($processor, $io);
         }
 
         $this->entityManagerInterface->flush();
@@ -43,54 +44,89 @@ class CPUToChipCommand extends Command
         return Command::SUCCESS;
     }
 
-    protected function toJson(Processor $processor) {
+    protected function toJson(Processor $processor, SymfonyStyle $io) {
+        $quadfsb = [400, 533, 667, 1066, 1333, 1600];
+        $doublefsb = [200, 266, 333, 400];
+        $htfsb = [800, 1000, 1800, 2000, 2200, 2400];
         $json = [];
         $json["Microarchitecture"] = [];
         $json["Power"] = [];
         $json["Microarchitecture"]["Core"] = $processor->getCore();
-        $json["Microarchitecture"]["Speed"] = $processor->getSpeed()?->getValueWithUnit();
-        if ($processor->getSpeed() && $processor->getFsb()) {
-            //Adapting the multiplier to the closest plausible value (may not always be satisfying)
-            $value = round($processor->getSpeed()->getValue() / $processor->getFsb()->getValue(), 1);
-            $flooredValue = floor($value);
-            if ($value < ($flooredValue + 0.2)) {
-                $value = $flooredValue;
-            } elseif ($value < ($flooredValue + 0.4)){
-                $value = $flooredValue + 0.25;
-            } elseif ($value < ($flooredValue + 0.6)) {
-                $value = $flooredValue + 0.5;
-            } elseif ($value < ($flooredValue + 0.8)) {
-                $value = $flooredValue + 0.75;
-            } else {
-                $value = $flooredValue + 1;
-            }
-            if($value - floor($value) == 0.25){
-                $value = $value * 2;
-            }
-            $json["Microarchitecture"]["Multiplier"] = $value . 'x';
-        }
+        $json["Microarchitecture"]["Frequency"] = $processor->getSpeed()?->getValueWithUnit();
         $json["Microarchitecture"]["Bus speed"] = $processor->getFsb()?->getValueWithUnit();
-        $json["Microarchitecture"]["Core count"] = $processor->getCores();
-        $json["Microarchitecture"]["Thread count"] = $processor->getThreads();
+        if ($processor->getSpeed() && $processor->getFsb()) {
+            $manufId = $processor->getManufacturer()->getId();
+            $speed = $processor->getSpeed()->getValue();
+            $commonFsb = $processor->getFsb()->getValue();
+            if(in_array($commonFsb, $quadfsb)){
+                $commonFsb = $commonFsb / 4;
+            }
+            if(in_array($commonFsb, $doublefsb)){
+                $commonFsb = $commonFsb / 2;
+            }
+            //treating common fsbs between vendors
+            if($manufId == 207){
+                //intel
+                if($commonFsb == 800){
+                    $commonFsb = $commonFsb / 4;
+                }
+                if($commonFsb == 2400){ //QPI
+                    $commonFsb = 133;
+                }
+                if($commonFsb == 2500){ //ivy
+                    $commonFsb = 100;
+                }
+            }
+            if($manufId == 27){
+                //amd
+                if(in_array($commonFsb, $htfsb)){
+                    $commonFsb = 200;
+                }
+            }
+
+            if($commonFsb == 33 && $speed > 100){
+                $commonFsb = 0;
+            }
+            if($commonFsb != 0){
+                $value = round($speed / $commonFsb, 1);
+                $flooredValue = floor($value);
+                if($value - floor($value) != 0.5 && $value - floor($value) != 0 && $value - floor($value) != 1){
+                    $value = $value - 0.1;
+                }
+                if($value - floor($value) != 0.5 && $value - floor($value) != 0 && $value - floor($value) != 1){
+                    $io->writeln($processor->getId() . " mul: " . $value . " freq" . $processor->getSpeed()->getValue() . " fsb" . $processor->getFsb()->getValue() . " manuf id " . $manufId);
+                }
+                $json["Microarchitecture"]["Multiplier"] = $value . 'x';
+            }
+        }
+        $cores = $processor->getCores();
+        if($cores > 1){
+            $json["Microarchitecture"]["Core count"] = $cores;
+            $json["Microarchitecture"]["Thread count"] = $processor->getThreads();
+        }
         if ($processor->getL2() && $processor->isL2shared()) {
             $json["Microarchitecture"]["L2 cache"] = $processor->getL2()->getValueWithUnit() . ' shared';
         } elseif ($processor->getL2() && $processor->getCores()) {
             $json["Microarchitecture"]["L2 cache"] = $processor?->getCores() . ' x ' . $processor->getL2()?->getValueWithUnit();
         } else {
-            $json["Microarchitecture"]["L2 cache"] = $processor->getL2()?->getValueWithUnit();
+            if($processor->getL2()){
+                $json["Microarchitecture"]["L2 cache"] = $processor->getL2()->getValueWithUnit();
+            }
         }
         if ($processor->getL3() && $processor->isL3shared()) {
             $json["Microarchitecture"]["L3 cache"] = $processor->getL3()->getValueWithUnit() . ' shared';
         } elseif ($processor->getL3() && $processor->getCores()) {
             $json["Microarchitecture"]["L3 cache"] = $processor->getCores() . ' x ' . $processor->getL3()->getValueWithUnit();
         } else {
-            $json["Microarchitecture"]["L3 cache"] = $processor->getL3()?->getValueWithUnit();
+            if($processor->getL2()){
+                $json["Microarchitecture"]["L3 cache"] = $processor->getL3()->getValueWithUnit();
+            }
         }
 
         $json["Power"]["Voltage"] = $processor->getVoltagesWithValue();
 
-        //$processor->setMiscSpecs($json);
+        $processor->setMiscSpecs($json);
 
-        //$this->entityManagerInterface->persist($processor);
+        $this->entityManagerInterface->persist($processor);
     }
 }
