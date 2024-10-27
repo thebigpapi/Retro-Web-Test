c281b656ef563a3a25f18bf8a32187013963a8e3
computerguy096 <computerguy096@gmail.com>
2024-10-27 15:29:49 -0400

    improved bios bulk uploading

--- assets/js/admin/editor_bios.js
+++ assets/js/admin/editor_bios.js
@@ -1,19 +1,27 @@
 if(readfile = document.getElementById('bulk-upload-btn'))
-    readfile.addEventListener("click", bulkUpload);
+    readfile.addEventListener("click", bulkUploadPrep);
 let manufArray = {};
 function setMsg(msg){
     if(label = document.getElementById('bios-form-label'))
         label.innerHTML = msg;
 }
 function getManufacturers(){
+    
+}
+function bulkUploadPrep(){
     let url = window.location.origin + "/dashboard/getbiosmanufacturers";
     let xhr = new XMLHttpRequest()
     xhr.open('GET', url, true)
     xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
     xhr.send();
+    setMsg("Please wait ...");
     xhr.onload = function () {
         if(xhr.status === 200) {
             manufArray = JSON.parse(xhr.responseText);
+            bulkUpload()
+        }
+        else{
+            setMsg("Error " + xhr.status + ": " + xhr.statusText);
         }
     }
 }
@@ -21,126 +29,75 @@ function bulkUpload(){
     try{
         getManufacturers();
         let add = document.getElementById('Motherboard_motherboardBios_collection').previousElementSibling;
-        let input = document.getElementById('bios-bulk-upload');
-        let bioses = document.getElementById('Motherboard_motherboardBios');
-        if(!bioses){
-            bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0];
-            if(bioses.innerHTML != "Empty"){
-                bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0].children[0].children[0];
-            }
-        }
-        if(bioses.childElementCount != 0){
-            setMsg("Error: BIOS section must be empty!");
+        let bulkInput = document.getElementById('bios-bulk-upload');
+        let csvInput = document.getElementById('bios-bot-input');
+        let csvReader = new FileReader();
+        let bioses = document.getElementsByClassName("Motherboard_motherboardBios_cssid");
+        let oldBioses = [...bioses];
+        let currentLength = bioses.length;
+        if(bulkInput.files.length == 0){
+            setMsg("Error: No BIOS files have been selected!");
             return;
         }
-        if(input.files.length == 0){
-            setMsg("Error: No files have been selected!");
-            return;
-        }
-        //console.log(input.files);
-        let cnt = 1;
-        for (let i = 0; i < input.files.length; i++) {
+        for (let i = 0; i < bulkInput.files.length; i++) {
             add.click();
-            const file = input.files[i]
-            const dt = new DataTransfer();
-            bios = document.getElementById('Motherboard_motherboardBios_' + cnt + '_romFile_file');
-            while(!bios){
-                cnt++;
-                bios = document.getElementById('Motherboard_motherboardBios_' + cnt + '_romFile_file');
-            }
-            label = document.getElementById('Motherboard_motherboardBios_' + cnt + '_romFile_file_new_file_name');
-            label.innerHTML = input.files[i].name;
-            dt.items.add(file);
-            bios.files = dt.files;
-            cnt++;
         }
-        let csvinput = document.getElementById('bios-bot-input');
-        if(input.files[0]){
-            readFile()
-        }
-    }
-    catch(error){
-        setMsg(error);
-    }
-}
-
-function readFile(){
-    try{
-        let fr=new FileReader();
-        let input = document.getElementById('bios-bot-input');
-        fr.onload=function(){
-            csv=csvToArray(fr.result);
-            if(csv.length < 2){
-                setMsg("Error: CSV is empty or invalid!");
+        let newBioses = [...bioses].filter(x => !oldBioses.includes(x));
+        currentLength = bioses.length;
+        if(csvInput.files.length > 0){
+            let csvExtension = csvInput.files[0].name.split('.').pop().toLowerCase();
+            if(csvExtension != "csv"){
+                setMsg("Error: not a CSV file!");
                 return;
             }
-            processFile(csv);
-        }
-        if(!input.files[0]){
-            setMsg("Warning: no CSV file selected!");
-            return;
-        }
-        let extension = input.files[0].name.split('.').pop().toLowerCase();
-        if(extension != "csv"){
-            setMsg("Error: not a CSV file!");
-            return;
-        }
-        if(confirm("BIOS strings are about to be changed, proceed?"))
-            fr.readAsText(input.files[0]);
-        else
-            return;
-    }
-    catch(error){
-        setMsg(error);
-    }
-}
-
-function processFile(csv){
-    try{
-        let bioses = document.getElementById('Motherboard_motherboardBios');
-        if(!bioses){
-            bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0];
-            if(bioses.innerHTML == "Empty"){
-                setMsg("Error: no BIOSes added!");
-                return;
-            }
-            bioses = document.getElementById('Motherboard_motherboardBios_collection').children[0].children[0].children[0].children[0];
-        }
-        if(bioses.childElementCount == 0){
-            setMsg("Error: no BIOSes added!");
-            return;
-        }
-        if(bioses.childElementCount < csv.length){
-            setMsg("Error: there are more BIOSes in the CSV than in the editor!\nCSV: " + csv.length + "\neditor: " + bioses.childElementCount);
-            return;
-        }
-        let bios_cnt = bioses.childElementCount;
-        let cnt = 0;
-        while(bios_cnt > 0){
-            //read file name
-            if(file = document.getElementById("Motherboard_motherboardBios_" + cnt + "_romFile_file_new_file_name")){
-                let name = file.innerHTML.split(" (")[0];
-                if(name == ""){
-                    if(link = document.getElementById("Motherboard_motherboardBios_" + cnt + "_romFile_file_link"))
-                        name = link.getAttribute('title');
+            csvReader.onload=function(){
+                let csv = csvToArray(csvReader.result);
+                if(csv.length < 2){
+                    setMsg("Error: CSV is empty or invalid!");
+                    return;
                 }
-                if(name != ""){
-                    let found = csv.find(item => item[0].includes(name));
-                    //console.log(name, found);
+                for(let i = 0; i < newBioses.length; i++){
+                    let found = csv.find(item => item[0].includes(bulkInput.files[i].name));
                     if(found){
-                        document.getElementById("Motherboard_motherboardBios_" + cnt + "_postString").value = found[3].slice(1,-1);
-                        document.getElementById("Motherboard_motherboardBios_" + cnt + "_boardVersion").value = found[4].slice(1,-1);
-                        document.getElementById("Motherboard_motherboardBios_" + cnt + "_coreVersion").value = found[2].slice(1,-1);
-                        manufacturer = found[1].slice(1,-1);
-                        let select = document.getElementById("Motherboard_motherboardBios_" + cnt + "_manufacturer_autocomplete");
+                        newBioses[i].querySelector("input[id*='postString']").value = found[3].slice(1,-1);
+                        newBioses[i].querySelector("input[id*='boardVersion']").value = found[4].slice(1,-1);
+                        let coreVersion = found[2].slice(1,-1);
+                        if(coreVersion.startsWith('v4') || coreVersion.startsWith('v6')){
+                            coreVersion = coreVersion.substring(1);
+                        }
+                        newBioses[i].querySelector("input[id*='coreVersion']").value = coreVersion;
+                        let manufacturer = found[1].slice(1,-1);
+                        let select = newBioses[i].querySelector("select[id*='manufacturer_autocomplete']");
                         select.tomselect.addOption({entityId: manufArray[manufacturer], entityAsString: manufacturer});
                         select.tomselect.addItem(manufArray[manufacturer]);
                         select.tomselect.sync();
                     }
                 }
-                bios_cnt--;
+                if(newBioses.length > 0){
+                    if(newBioses.length < csv.length){
+                        setMsg("Warning: Added only " + newBioses.length + " BIOS files [CSV: " + csv.length + ", provided files: " + newBioses.length + "]");
+                    }
+                    else{
+                        setMsg("Added " + newBioses.length + " BIOS files");
+                    }
+                }
+                else{
+                    setMsg("Error: No BIOSes added!");
+                }
             }
-            cnt++;
+            csvReader.readAsText(csvInput.files[0]);
+        }
+        else{
+            setMsg("Warning: Added " + newBioses.length + " files without CSV processing!");
+        }
+        for(let i = 0; i < newBioses.length; i++){
+            const file = bulkInput.files[i]
+            const dt = new DataTransfer();
+            let bios = newBioses[i].querySelector('input[type=file]');
+            let label = newBioses[i].querySelector("div[id*='romFile_file_new_file_name']");
+            label.innerHTML = bulkInput.files[i].name;
+            dt.items.add(file);
+            bios.files = dt.files;
         }
     }
     catch(error){
