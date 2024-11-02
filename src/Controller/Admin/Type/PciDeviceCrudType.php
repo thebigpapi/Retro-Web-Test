3f4642459581648b3dbdb6f02a0619c1eba5ae03
deksor <deksor@gmail.com>
2024-11-02 23:20:50 +0100

    Fixed PCI id bug

--- src/Controller/Admin/Type/PciDeviceCrudType.php
+++ src/Controller/Admin/Type/PciDeviceCrudType.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Admin\Type;
 
+use App\EasyAdmin\HexField;
 use App\Entity\PciDeviceId;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,9 +19,37 @@ class PciDeviceCrudType extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield TextField::new('dev')
+        yield HexField::new('dev')
             ->setFormTypeOption('label',false)
             ->setFormTypeOption('attr',['placeholder' => 'Device ID:'])
             ->setColumns(12);
     }
+
+    /*public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setNumberFormat('hex');
+    }*/
+
+    public function new(AdminContext $context)
+    {
+        dd($context->getEntity());
+    }
+
+    public function edit(AdminContext $context)
+    {
+        dd($context->getEntity());
+    }
+
+    public function delete(AdminContext $context)
+    {
+        dd($context->getEntity());
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        dd($entityInstance);
+    }
+
+
 }
